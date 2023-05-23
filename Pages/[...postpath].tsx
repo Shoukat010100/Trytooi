b725ce0372bc98cdbb62342118c6330e96ae2150
Shoukat010100 <134385276+Shoukat010100@users.noreply.github.com>
2023-05-23 21:27:26 +0500

    Update [...postpath].tsx

--- Pages/[...postpath].tsx
+++ Pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = ;https://trytooi.000webhostapp.com/graphql;
+	const endpoint = https://trytooi.000webhostapp.com/graphql;
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
